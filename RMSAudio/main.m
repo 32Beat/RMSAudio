8a0470826a4e507e7ee9c4271721fe683e6e5535
32BT <32BT@32bt.home>
2016-06-23 12:19:24 +0200

    Initial Commit

---
+++ RMSAudio/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  RMSAudio
+//
+//  Created by 32BT on 23/06/16.
+//  Copyright © 2016 32BT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}

24378c4051ed537a23186ebd9354c7d139ca9662
32BT <32BT@32bt.home>
2016-07-03 13:13:41 +0200

    RMSVarispeed decimation/interpolation

--- RMSAudio/RMSAudio/RMSSource/RMSSourceTypes/RMSVarispeed/RMSVarispeed.m
+++ RMSAudio/RMSAudio/RMSSource/RMSSourceTypes/RMSVarispeed/RMSVarispeed.m
@@ -33,13 +33,13 @@
 @implementation RMSVarispeed
 ////////////////////////////////////////////////////////////////////////////////
 
-static OSStatus fetchSampleNN(void *rmsObject, UInt64 index, Float32 *ptrL, Float32 *ptrR)
+static OSStatus PrepareFetch(void *rmsObject, UInt64 index)
 {
 	OSStatus result = noErr;
 
 	__unsafe_unretained RMSVarispeed *rmsSource = \
 	(__bridge __unsafe_unretained RMSVarispeed *)rmsObject;
-
+	
 	if (index >= rmsSource->mSrcListIndex+rmsSource->mSrcListCount)
 	{
 		rmsSource->mSrcSamplesL[0] = rmsSource->mSrcSamplesL[512];
@@ -56,6 +56,9 @@ static OSStatus fetchSampleNN(void *rmsObject, UInt64 index, Float32 *ptrL, Floa
 		info.frameIndex = index&(~511);
 		info.frameCount = 512;
 		info.bufferListPtr = (AudioBufferList *)&rmsSource->mSrcList;
+
+		rmsSource->mSrcList.buffer[0].mData = &rmsSource->mSrcSamplesL[4]; // 2+2 padding
+		rmsSource->mSrcList.buffer[1].mData = &rmsSource->mSrcSamplesR[4]; // 2+2 padding
 		
 		result = RunRMSSource((__bridge void *)rmsSource->mSource, &info);
 		if (result != noErr)
@@ -65,11 +68,150 @@ static OSStatus fetchSampleNN(void *rmsObject, UInt64 index, Float32 *ptrL, Floa
 		rmsSource->mSrcListCount = info.frameCount;
 	}
 	
-	index &= 511;
-	index += 2;
+	return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static OSStatus DecimateSource(void *rmsObject, const RMSCallbackInfo *infoPtr)
+{
+	OSStatus result = noErr;
+
+	__unsafe_unretained RMSVarispeed *rmsSource = \
+	(__bridge __unsafe_unretained RMSVarispeed *)rmsObject;
+
+	Float32 *dstPtrL = infoPtr->bufferListPtr->mBuffers[0].mData;
+	Float32 *dstPtrR = infoPtr->bufferListPtr->mBuffers[1].mData;
+
+	Float64 srcIndex = rmsSource->mSrcIndex;
+	Float64 srcStep = rmsSource->mSrcStep;
+
+	for (UInt32 n=0; n!=infoPtr->frameCount; n++)
+	{
+		Float64 L = 0.0;
+		Float64 R = 0.0;
+		Float64 s = 0.0;
+		
+		UInt64 index = srcIndex;
+
+		Float64 x = srcIndex - trunc(srcIndex);
+		if (x > 0.0)
+		{
+			result = PrepareFetch(rmsObject, index);
+			if (result != noErr) return result;
+
+			L += (1.0-x) * rmsSource->mSrcSamplesL[index&511];
+			R += (1.0-x) * rmsSource->mSrcSamplesR[index&511];
+			s += (1.0-x);
+			x -= 1.0;
+		}
+
+		x += srcStep;
+		while (x >= 1.0)
+		{
+			index += 1;
+			
+			result = PrepareFetch(rmsObject, index);
+			if (result != noErr) return result;
+
+			L += rmsSource->mSrcSamplesL[index&511];
+			R += rmsSource->mSrcSamplesR[index&511];
+			s += 1.0;
+			x -= 1.0;
+		}
+
+		if (x > 0.0)
+		{
+			index += 1;
+			
+			result = PrepareFetch(rmsObject, index);
+			if (result != noErr) return result;
+
+			L += x * rmsSource->mSrcSamplesL[index&511];
+			R += x * rmsSource->mSrcSamplesR[index&511];
+			s += x;
+		}
+
+		dstPtrL[n] = L/s;
+		dstPtrR[n] = R/s;
+		
+		srcIndex += srcStep;
+	}
+
+	rmsSource->mSrcIndex = srcIndex;
 	
-	ptrL[0] = rmsSource->mSrcSamplesL[index];
-	ptrR[0] = rmsSource->mSrcSamplesR[index];
+	return noErr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+static inline double Bezier(Float32 *srcPtr, Float64 t)
+{
+	double P0 = srcPtr[0];
+	double P1 = srcPtr[1];
+	double P2 = srcPtr[2];
+	double P3 = srcPtr[3];
+	double C1 = P1+0.25*(P2-P0);
+	double C2 = P2-0.25*(P3-P1);
+	
+	P1 += t * (C1-P1);
+	C1 += t * (C2-C1);
+	C2 += t * (P2-C2);
+
+	P1 += t * (C1-P1);
+	C1 += t * (C2-C1);
+
+	P1 += t * (C1-P1);
+	
+	return P1;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static void interpolate(
+AudioBufferList *srcList, double srcIndex,
+AudioBufferList *dstList, UInt64 dstIndex, UInt32 bufferCount)
+{
+	SInt64 index = srcIndex;
+	index &= 511;
+	index -= 2; // offset for srcList
+	index -= 1; // offset for Bezier
+
+	double t = srcIndex - trunc(srcIndex);
+	
+	for (UInt32 n=0; n!=bufferCount; n++)
+	{
+		Float32 *srcPtr = srcList->mBuffers[n].mData;
+		Float32 *dstPtr = dstList->mBuffers[n].mData;
+		
+		dstPtr[dstIndex] = Bezier(&srcPtr[index], t);
+	}
+}
+
+static OSStatus InterpolateSource(void *rmsObject, const RMSCallbackInfo *infoPtr)
+{
+	OSStatus result = noErr;
+
+	__unsafe_unretained RMSVarispeed *rmsSource = \
+	(__bridge __unsafe_unretained RMSVarispeed *)rmsObject;
+
+	Float64 srcIndex = rmsSource->mSrcIndex;
+	Float64 srcStep = rmsSource->mSrcStep;
+	
+	for (UInt32 n=0; n!=infoPtr->frameCount; n++)
+	{
+		UInt64 index = srcIndex;
+		
+		result = PrepareFetch(rmsObject, index);
+		if (result != noErr) return result;
+		
+		interpolate((AudioBufferList *)&rmsSource->mSrcList, srcIndex, infoPtr->bufferListPtr, n, 2);
+		
+		srcIndex += srcStep;
+	}
+	
+	rmsSource->mSrcIndex = srcIndex;
 	
 	return result;
 }
@@ -82,14 +224,13 @@ static OSStatus renderCallback(void *rmsObject, const RMSCallbackInfo *infoPtr)
 	__unsafe_unretained RMSVarispeed *rmsSource = \
 	(__bridge __unsafe_unretained RMSVarispeed *)rmsObject;
 	
-	Float32 *dstPtrL = infoPtr->bufferListPtr->mBuffers[0].mData;
-	Float32 *dstPtrR = infoPtr->bufferListPtr->mBuffers[1].mData;
-	for (UInt32 n=0; n!=infoPtr->frameCount; n++)
+	if (rmsSource->mSrcStep < 1.0)
 	{
-		fetchSampleNN(rmsObject, round(rmsSource->mSrcIndex), dstPtrL, dstPtrR);
-		rmsSource->mSrcIndex += rmsSource->mSrcStep;
-		dstPtrL += 1;
-		dstPtrR += 1;
+		result = InterpolateSource(rmsObject, infoPtr);
+	}
+	else
+	{
+		result = DecimateSource(rmsObject, infoPtr);
 	}
 	
 	return result;
@@ -117,11 +258,11 @@ static OSStatus renderCallback(void *rmsObject, const RMSCallbackInfo *infoPtr)
 		mSrcListIndex = 0;
 		mSrcList.bufferCount = 2;
 		mSrcList.buffer[0].mNumberChannels = 1;
-		mSrcList.buffer[0].mDataByteSize = 516*sizeof(float);
-		mSrcList.buffer[0].mData = mSrcSamplesL+4; // 2+2 padding
+		mSrcList.buffer[0].mDataByteSize = 512*sizeof(float);
+		mSrcList.buffer[0].mData = &mSrcSamplesL[4]; // 2+2 padding
 		mSrcList.buffer[1].mNumberChannels = 1;
-		mSrcList.buffer[1].mDataByteSize = 516*sizeof(float);
-		mSrcList.buffer[1].mData = mSrcSamplesR+4; // 2+2 padding
+		mSrcList.buffer[1].mDataByteSize = 512*sizeof(float);
+		mSrcList.buffer[1].mData = &mSrcSamplesR[4]; // 2+2 padding
 	}
 	
 	return self;
@@ -156,6 +297,10 @@ static OSStatus renderCallback(void *rmsObject, const RMSCallbackInfo *infoPtr)
 	double dstRate = [self sampleRate];
 	
 	mSrcStep = dstRate ? srcRate / dstRate : 1.0;
+	if ((mSrcIndex == 0.0) && (mSrcStep < 1.0))
+	{
+		mSrcIndex += 0.5 * mSrcStep;
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
